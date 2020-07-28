f576c670a9422437e6709a536dd45f9fa55a847f
hari <nepal9841921783@gmail.com>
2020-07-28 21:44:53 +0100

    :art: added search Bar

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -2,14 +2,19 @@ import React from 'react';
 import '../bootstrap.min.css'
 import Navbar from "./Layout/Navbar";
 import Cards from "./Home/Cases Cards/Cards";
+import SearchBar from "./Home/Utils/SearchBar";
 
 const HomePage = () => {
     return (
         <body id="page-top">
         <div id="wrapper">
             <Navbar/>
+            <div className="d-flex flex-column" id="content-wrapper">
+                <div id="content">
+            <SearchBar/>
             <Cards/>
-
+                </div>
+            </div>
         </div>
         </body>
     );
