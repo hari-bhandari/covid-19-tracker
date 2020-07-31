82835ef8ffea0f9b6ea2f5b370ae3e7d921fc196
hari <nepal9841921783@gmail.com>
2020-07-31 13:30:18 +0100

    :fire: added local storage to load country

--- src/Components/Home/Select Component/ChooseCountry.js
+++ src/Components/Home/Select Component/ChooseCountry.js
@@ -1,18 +1,27 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import './select.css'
 import CovidContext from "../../../Context/COVID/covidContext";
 const ChooseCountry =  () => {
     const covidContext=useContext(CovidContext)
-    const{countries,changeCountry}=covidContext
+    const{countries,changeCountry,overall}=covidContext
     const[selectedCountry,setSelectedCountry]=useState('International')
+    useEffect(()=>{
+        if(countries){
+            setSelectedCountry(localStorage.getItem("selectedCountry"))
+            changeCountry(localStorage.getItem("selectedCountry"))
+        }
+    },[countries])
     const onChange=(e)=>{
+        localStorage.setItem("selectedCountry",e.target.value)
         setSelectedCountry(e.target.value)
         changeCountry(e.target.value)
-
     }
     return (
         <div className="select">
             <select name="format" id="format" onChange={onChange}>
+
+                { selectedCountry==='International'?'':(overall&&<option selected value={selectedCountry}>{overall.country}</option>)}
+
                 <option selected value={"International"}>International</option>
                 {countries&&countries.map((country,index)=>(
                     <option value={country.countryInfo.iso2} key={index}>{country.country}</option>
